9d9da58a21ec6dc4b72648373f1398a46f5ebf15
wellington <wellingtonfds@gmail.com>
2021-03-31 20:12:49 -0300

    add route guard

--- front-end/src/app/pages/pages-routing.module.ts
+++ front-end/src/app/pages/pages-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthService as AuthGuard } from '@services/guard/auth.service';
+
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class PagesRoutingModule { }
--- front-end/src/app/pages/pages.module.ts
+++ front-end/src/app/pages/pages.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginModule } from './login/login.module';
+import { HomeModule } from './home/home.module';
+
 
 @NgModule({
-  declarations: [LoginModule],
+  declarations: [LoginModule, HomeModule],
   imports: [
     CommonModule,
-    LoginModule
+    LoginModule,
+    HomeModule
   ]
 })
 export class PagesModule { }
