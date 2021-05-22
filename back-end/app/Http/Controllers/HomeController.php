950f19a59fa3ed69c61f7eb87ac1a1875709e902
wellington <wellingtonfds@gmail.com>
2021-05-21 23:45:55 -0300

    update home

--- back-end/app/Http/Controllers/HomeController.php
+++ back-end/app/Http/Controllers/HomeController.php
@@ -12,15 +12,29 @@ class HomeController extends Controller
     public function index()
     {
         $contracts = Contract::where('date_end', '>', new Carbon())->count();
+        $contractsInative = Contract::where('date_end', '<', new Carbon())->count();
+        $contractsDeadline = Contract::whereRaw('datediff(date_end, now()) <= ?', 15)
+            ->whereRaw('datediff(date_end, now()) >= ?', 15)
+            ->where('date_end', '>', Carbon::now())
+            ->count();
+        $contractsBirthday = Contract::whereRaw('datediff(readjustment_base_date, now()) <= ?', 15)
+            ->whereRaw('datediff(readjustment_base_date, now()) >= ?', 15)
+            ->where('date_end', '>', Carbon::now())
+            ->count();
         $proposalPending = Proposal::where('status', 'draft')->count();
         $proposalCommittee1 = Proposal::where('status', 'committee_1')->count();
         $proposalCommittee2 = Proposal::where('status', 'committee_2')->count();
+        $proposalWinner = Proposal::where('status', 'winner')->count();
 
         $data = [
             'contract_ative' => $contracts,
+            'contract_inative' => $contractsInative,
+            'contract_deadline' => $contractsDeadline,
+            'contract_birthday' => $contractsBirthday,
             'proposal_pending' => $proposalPending,
             'proposal_committee_1' => $proposalCommittee1,
-            'proposal_committee_2' => $proposalCommittee2
+            'proposal_committee_2' => $proposalCommittee2,
+            'proposal_winner' => $proposalWinner
         ];
 
         return $data;
--- front-end/src/app/models/home.model.ts
+++ front-end/src/app/models/home.model.ts
@@ -1,7 +1,11 @@
 
 export interface Home {
-    contract_ative: number;
+    proposal_winner: number;
     proposal_pending: number;
     proposal_committee_1: number;
     proposal_committee_2: number;
+    contract_ative: number;
+    contract_birthday: number;
+    contract_deadline: number;
+    contract_inative: number;
 }
